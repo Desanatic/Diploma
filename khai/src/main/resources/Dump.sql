cc585afcb5e45e0134251a32ed134e27acb2371d
Vladyslav_Dubinin@epam.com <Afrdfvdctv1994>
2015-12-27 23:25:49 +0200

    add Dump

--- khai/src/main/resources/Dump.sql
+++ khai/src/main/resources/Dump.sql
@@ -21,13 +21,14 @@ DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.group: ~0 rows (приблизительно)
+-- Дамп данных таблицы trkhai.group: ~1 rows (приблизительно)
 /*!40000 ALTER TABLE `group` DISABLE KEYS */;
 INSERT INTO `group` (`id`, `name`) VALUES
-	(0, 'aw');
+	(0, 'aw'),
+	(1, 'teacher');
 /*!40000 ALTER TABLE `group` ENABLE KEYS */;
 
 
@@ -35,19 +36,16 @@ INSERT INTO `group` (`id`, `name`) VALUES
 DROP TABLE IF EXISTS `homework`;
 CREATE TABLE IF NOT EXISTS `homework` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `training_id` int(11) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `date_of_delivery` date DEFAULT NULL,
-  KEY `id` (`id`),
-  KEY `FK_homework_training` (`training_id`),
-  CONSTRAINT `FK_homework_training` FOREIGN KEY (`training_id`) REFERENCES `training` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.homework: ~0 rows (приблизительно)
+-- Дамп данных таблицы trkhai.homework: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `homework` DISABLE KEYS */;
-INSERT INTO `homework` (`id`, `training_id`, `name`, `date_of_delivery`) VALUES
-	(0, 1, 'firsHomework', '2015-11-01'),
-	(2, 1, 'adawawd', '2015-12-20');
+INSERT INTO `homework` (`id`, `name`, `date_of_delivery`) VALUES
+	(0, 'firsHomework', '2015-11-01'),
+	(2, 'adawawd', '2015-12-20');
 /*!40000 ALTER TABLE `homework` ENABLE KEYS */;
 
 
@@ -60,14 +58,14 @@ CREATE TABLE IF NOT EXISTS `homework_task` (
   `task` text,
   `chart` text,
   `state` enum('DONE','CLOSE','IN_PROGRESS','OVERDUE','NEW') DEFAULT NULL,
-  KEY `id` (`id`),
+  PRIMARY KEY (`id`),
   KEY `FK_homework_task_homework` (`homework_id`),
   KEY `FK_homework_task_user` (`user_id`),
   CONSTRAINT `FK_homework_task_homework` FOREIGN KEY (`homework_id`) REFERENCES `homework` (`id`),
   CONSTRAINT `FK_homework_task_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.homework_task: ~1 rows (приблизительно)
+-- Дамп данных таблицы trkhai.homework_task: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `homework_task` DISABLE KEYS */;
 INSERT INTO `homework_task` (`id`, `homework_id`, `user_id`, `task`, `chart`, `state`) VALUES
 	(1, 0, 0, 'С одной стороны улицы подряд стоят пять домов, каждый — своего цвета. В каждом живёт человек, все пять — разных национальностей. Каждый человек предпочитает уникальную марку сигарет, напиток и домашнее животное. Кроме того:', '{labels: [\'Mon\', \'Tue\', \'Wed\', \'Thu\', \'Fri\'],', 'NEW'),
@@ -80,7 +78,7 @@ DROP TABLE IF EXISTS `proverb`;
 CREATE TABLE IF NOT EXISTS `proverb` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `proverb` varchar(100) DEFAULT NULL,
-  KEY `id` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- Дамп данных таблицы trkhai.proverb: ~10 rows (приблизительно)
@@ -103,16 +101,10 @@ INSERT INTO `proverb` (`id`, `proverb`) VALUES
 DROP TABLE IF EXISTS `solution`;
 CREATE TABLE IF NOT EXISTS `solution` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `homework_id` int(11) DEFAULT NULL,
   `decision` text,
   `chart` text,
   `assessment` int(11) DEFAULT NULL,
-  KEY `id` (`id`),
-  KEY `FK_solution_user` (`user_id`),
-  KEY `FK_solution_homework` (`homework_id`),
-  CONSTRAINT `FK_solution_homework` FOREIGN KEY (`homework_id`) REFERENCES `homework` (`id`),
-  CONSTRAINT `FK_solution_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Дамп данных таблицы trkhai.solution: ~0 rows (приблизительно)
@@ -120,18 +112,40 @@ CREATE TABLE IF NOT EXISTS `solution` (
 /*!40000 ALTER TABLE `solution` ENABLE KEYS */;
 
 
+-- Дамп структуры для таблица trkhai.teacher
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE IF NOT EXISTS `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `training_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_teacher_user` (`user_id`),
+  KEY `FK_teacher_training` (`training_id`),
+  CONSTRAINT `FK_teacher_training` FOREIGN KEY (`training_id`) REFERENCES `training` (`id`),
+  CONSTRAINT `FK_teacher_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- Дамп данных таблицы trkhai.teacher: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` (`id`, `user_id`, `training_id`) VALUES
+	(1, 1, 1),
+	(2, 1, 2);
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+
+
 -- Дамп структуры для таблица trkhai.training
 DROP TABLE IF EXISTS `training`;
 CREATE TABLE IF NOT EXISTS `training` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
-  KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.training: ~0 rows (приблизительно)
+-- Дамп данных таблицы trkhai.training: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `training` DISABLE KEYS */;
 INSERT INTO `training` (`id`, `name`) VALUES
-	(1, 'testTraining');
+	(1, 'testTraining'),
+	(2, 'testTraining2');
 /*!40000 ALTER TABLE `training` ENABLE KEYS */;
 
 
@@ -143,17 +157,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `name` varchar(50) DEFAULT NULL,
   `surname` varchar(50) DEFAULT NULL,
   `middle_name` varchar(50) DEFAULT NULL,
-  `role` varchar(50) DEFAULT NULL,
+  `role` enum('TEACHER','STUDENT') DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  KEY `id` (`id`),
+  PRIMARY KEY (`id`),
   KEY `FK_user_group` (`group_id`),
   CONSTRAINT `FK_user_group` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.user: ~0 rows (приблизительно)
+-- Дамп данных таблицы trkhai.user: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `group_id`, `name`, `surname`, `middle_name`, `role`, `password`) VALUES
-	(0, 0, 'Vladyslav', 'Dubinin', 'Artemovich', 'student', '123456');
+	(0, 0, 'Vladyslav', 'Dubinin', 'Artemovich', 'STUDENT', '123456'),
+	(1, 1, 'user', '1', 'user', 'TEACHER', '123456');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 
@@ -165,22 +180,25 @@ CREATE TABLE IF NOT EXISTS `user_home_work` (
   `home_work_id` int(11) DEFAULT NULL,
   `home_work_task_id` int(11) DEFAULT NULL,
   `training_id` int(11) DEFAULT NULL,
-  KEY `id` (`id`),
+  `solution_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `FK_user_home_work_user` (`user_id`),
   KEY `FK_user_home_work_homework` (`home_work_id`),
   KEY `FK_user_home_work_training` (`training_id`),
   KEY `FK_user_home_work_homework_task` (`home_work_task_id`),
+  KEY `FK_user_home_work_solution` (`solution_id`),
   CONSTRAINT `FK_user_home_work_homework` FOREIGN KEY (`home_work_id`) REFERENCES `homework` (`id`),
   CONSTRAINT `FK_user_home_work_homework_task` FOREIGN KEY (`home_work_task_id`) REFERENCES `homework_task` (`id`),
+  CONSTRAINT `FK_user_home_work_solution` FOREIGN KEY (`solution_id`) REFERENCES `solution` (`id`),
   CONSTRAINT `FK_user_home_work_training` FOREIGN KEY (`training_id`) REFERENCES `training` (`id`),
   CONSTRAINT `FK_user_home_work_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- Дамп данных таблицы trkhai.user_home_work: ~0 rows (приблизительно)
+-- Дамп данных таблицы trkhai.user_home_work: ~2 rows (приблизительно)
 /*!40000 ALTER TABLE `user_home_work` DISABLE KEYS */;
-INSERT INTO `user_home_work` (`id`, `user_id`, `home_work_id`, `home_work_task_id`, `training_id`) VALUES
-	(1, 0, 0, 1, 1),
-	(3, 0, 0, 2, 1);
+INSERT INTO `user_home_work` (`id`, `user_id`, `home_work_id`, `home_work_task_id`, `training_id`, `solution_id`) VALUES
+	(1, 0, 0, 1, 2, NULL),
+	(3, 0, 0, 2, 1, NULL);
 /*!40000 ALTER TABLE `user_home_work` ENABLE KEYS */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
